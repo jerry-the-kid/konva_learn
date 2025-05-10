89407244e7db30f49f539c18aa2f80d38764f713
Hao Pham <jerrycarry79@gmail.com>
2025-05-10 17:53:46 +0700

    test completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,412 +1,244 @@
-import { useEffect, useRef, useState } from 'react';
-import { Image, Layer, Stage, Text, Transformer } from 'react-konva';
-import useImage from 'use-image';
-import './App.css';
-import AutosizeInput from 'react-input-autosize';
+import Konva from 'konva';
+import { useRef, useState } from 'react';
+import { Group, Layer, Rect, Stage, Text } from 'react-konva';
 
-type Image = {
+const CELL_WIDTH = 200;
+const CELL_HEIGHT = 150;
+const GRID_ROWS = 4;
+const GRID_COLS = 4;
+const GRID_GAP = 10;
+const GRID_PADDING_HORIZONTAL = 40;
+const GRID_PADDING_VERTICAL = 80;
+
+type Product = {
   id: number;
-  src: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  name: string;
+  price: string;
 };
 
-type CanvasText = {
-  id: number;
-  text: string;
+const productList: Product[] = [
+  { id: Math.random(), name: 'Heineken 0.0 | 330 ml', price: '10,10 £' },
+  { id: Math.random(), name: 'Heineken Original | 330 ml', price: '10,10 £' },
+  { id: Math.random(), name: 'Heineken Silver | 330 ml', price: '10,10 £' },
+];
+
+type Coord = {
   x: number;
   y: number;
-  fontSize: number;
 };
 
-interface URLImageProps {
-  image: Image;
-  isSelectedImage: boolean;
-  onSelectImage: VoidFunction;
-  onCoordsChange: ({ x, y }: { x: number; y: number }) => void;
-}
+type KonvaItem = Product & Coord;
 
-function downloadURI(uri, name) {
-  var link = document.createElement('a');
-  link.download = name;
-  link.href = uri;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-}
-
-const URLImage = ({
-  image,
-  isSelectedImage,
-  onSelectImage,
-  onCoordsChange,
-}: URLImageProps) => {
-  const [img] = useImage(image.src, 'anonymous');
-  const trRef = useRef<any>();
-  const imageRef = useRef<any>();
-
-  useEffect(() => {
-    if (isSelectedImage) {
-      trRef.current.nodes([imageRef.current]);
-      trRef.current.getLayer().batchDraw();
-    }
-  }, [isSelectedImage]);
-
-  return (
-    <>
-      <Image
-        onClick={onSelectImage}
-        onTap={onSelectImage}
-        ref={imageRef}
-        image={img}
-        x={image.x}
-        y={image.y}
-        width={image.width}
-        height={image.height}
-        // offsetX={img ? img.width / 2 : 0}
-        // offsetY={img ? img.height / 2 : 0}
-        draggable
-        onDragEnd={(e) => {
-          onCoordsChange({
-            x: e.target.x(),
-            y: e.target.y(),
-          });
-        }}
-      />
-      {isSelectedImage && (
-        <Transformer
-          ref={trRef}
-          rotateEnabled={false}
-          enabledAnchors={[
-            'top-left',
-            'top-right',
-            'bottom-left',
-            'bottom-right',
-          ]}
-          boundBoxFunc={(oldBox, newBox) => {
-            // limit resize to a minimum size
-            if (newBox.width < 50 || newBox.height < 50) {
-              return oldBox;
-            }
-            return newBox;
-          }}
-        />
-      )}
-    </>
+function calculateOverlap(
+  itemX: number,
+  itemY: number,
+  gridX: number,
+  gridY: number,
+) {
+  const xOverlap = Math.max(
+    0,
+    Math.min(itemX + CELL_WIDTH, gridX + CELL_WIDTH) - Math.max(itemX, gridX),
   );
-};
+  const yOverlap = Math.max(
+    0,
+    Math.min(itemY + CELL_HEIGHT, gridY + CELL_HEIGHT) - Math.max(itemY, gridY),
+  );
 
-const createRandomId = () => Math.random() * 10;
-const MAX_WIDTH = 300;
-const MAX_HEIGHT = 300;
+  const overlapArea = xOverlap * yOverlap;
+  const rectArea = CELL_WIDTH * CELL_HEIGHT;
 
-const App = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const dragUrlRef = useRef<string>();
-  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const percentage = (overlapArea / rectArea) * 100;
+  return percentage;
+}
 
-  const [images, setImages] = useState<Image[]>([]);
-  const [selectedImage, setSelectedImage] = useState(0);
+export default function App() {
+  const stageRef = useRef<Konva.Stage>(null);
+  const [menuItems, setMenuItems] = useState<Product[]>(productList);
+  const [konvaItems, setKonvaItems] = useState<KonvaItem[]>([]);
+  const snapCoords = useRef<Coord>({ x: 0, y: 0 });
 
-  const [texts, setTexts] = useState<CanvasText[]>([]);
-  const [selectedTextId, setSelectedTextId] = useState<number | null>(null);
-  const [editedTextValue, setEditedTextValue] = useState({
-    textValue: '',
-    x: 50,
-    y: 50,
-  });
-  const [selectedEditTextId, setSelectedEditTextId] = useState(0);
+  const coords: Coord[] = Array.from(
+    { length: GRID_ROWS * GRID_COLS },
+    (_, i) => {
+      const row = Math.floor(i / GRID_COLS);
+      const col = i % GRID_COLS;
+      const x = col * (CELL_WIDTH + GRID_GAP) + GRID_PADDING_HORIZONTAL;
+      const y = row * (CELL_HEIGHT + GRID_GAP) + GRID_PADDING_VERTICAL;
+      return { x, y };
+    },
+  );
 
-  const [imageSources, setImageSources] = useState<string[]>([]);
+  const canvasWidth =
+    CELL_WIDTH * GRID_COLS +
+    GRID_GAP * (GRID_COLS - 1) +
+    GRID_PADDING_HORIZONTAL * 2;
 
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Delete') {
-        if (selectedImage !== 0) {
-          setImages((prev) => prev.filter((img) => img.id !== selectedImage));
-          setSelectedImage(0);
+  const canvasHeight =
+    CELL_HEIGHT * GRID_ROWS +
+    GRID_GAP * (GRID_ROWS - 1) +
+    GRID_PADDING_VERTICAL * 2;
+
+  const getPerfectDropCoords = (x: number, y: number): Coord => {
+    const { maxCoord } = coords.reduce(
+      (acc, coord) => {
+        const overlap = calculateOverlap(x, y, coord.x, coord.y);
+        if (overlap > acc.maxValue) {
+          return {
+            maxValue: overlap,
+            maxCoord: coord,
+          };
         }
-
-        if (selectedTextId !== null) {
-          setTexts((prev) => prev.filter((txt) => txt.id !== selectedTextId));
-          setSelectedTextId(null);
-        }
-      }
-    };
-
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [selectedImage, selectedTextId]);
-
-  useEffect(() => {
-    const updateSize = () => {
-      if (containerRef.current) {
-        setDimensions({
-          width: containerRef.current.clientWidth,
-          height: containerRef.current.clientHeight,
-        });
-      }
-    };
-
-    updateSize(); // Initial call
-
-    window.addEventListener('resize', updateSize);
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-
-  const stageRef = useRef<any>();
-
-  const handleExport = () => {
-    const uri = stageRef.current.toDataURL();
-
-    // we also can save uri as file
-    downloadURI(uri, 'stage.png');
+        return acc;
+      },
+      {
+        maxValue: -Infinity,
+        maxCoord: { x: 0, y: 0 },
+      },
+    );
+    return maxCoord;
   };
 
-  const inputRef = useRef<HTMLInputElement | null>(null);
+  const handleDragEnd = (
+    e: Konva.KonvaEventObject<DragEvent>,
+    index: number,
+  ) => {
+    const { x, y } = snapCoords.current;
+    setKonvaItems((prevItems) => {
+      const updatedItems = [...prevItems];
+
+      const overlapItemIndex = updatedItems.findIndex(
+        (item) => Math.trunc(item.x) === x && Math.trunc(item.y) === y,
+      );
+
+      if (overlapItemIndex !== -1 && overlapItemIndex !== index) {
+        updatedItems[overlapItemIndex] = {
+          ...updatedItems[overlapItemIndex],
+          x: updatedItems[index].x,
+          y: updatedItems[index].y,
+        };
+      }
+
+      updatedItems[index] = {
+        ...updatedItems[index],
+        x: x + Math.random() * 0.001,
+        y: y + Math.random() * 0.001,
+      };
+      return updatedItems;
+    });
+  };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        paddingTop: '10rem',
-        paddingInline: '5rem',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'start',
-          gap: '10px',
-        }}
-      >
-        <button onClick={handleExport}>
-          Click here to export stage as image
-        </button>
-        <button
-          onClick={() => {
-            const id = Math.random();
-            setTexts((prev) => [
-              ...prev,
-              { id, text: 'New Text', x: 50, y: 50, fontSize: 24 },
-            ]);
-            setSelectedTextId(id);
-          }}
-        >
-          Add Text
-        </button>
-
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) => {
-            const file = e.target.files?.[0];
-            if (!file) return;
-
-            const reader = new FileReader();
-            reader.onload = () => {
-              setImageSources((prev) => [...prev, reader.result as string]);
-            };
-            reader.readAsDataURL(file);
-          }}
-        />
-        {imageSources.map((src, index) => (
-          <img
-            key={index}
-            alt="draggable"
-            src={src}
-            width={100}
-            style={{ margin: '10px', border: '1px solid #ccc', cursor: 'grab' }}
-            draggable="true"
-            onDragStart={() => {
-              dragUrlRef.current = src;
-            }}
-          />
-        ))}
-      </div>
-      <div
-        ref={containerRef}
-        style={{
-          width: '1050px',
-          height: '550px',
-          border: '1px solid black',
-          margin: '50px auto',
-          position: 'relative',
-        }}
-        onDrop={(e) => {
-          e.preventDefault();
-          const imageUrl = dragUrlRef.current;
-          if (!containerRef.current || !imageUrl) return;
-
-          const rect = containerRef.current.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-
-          const id = createRandomId();
-          const img = new window.Image();
-
-          img.src = imageUrl;
-          img.onload = () => {
-            const { width, height } = img;
-
-            // Calculate scale factor
-            const scale = Math.min(MAX_WIDTH / width, MAX_HEIGHT / height, 1);
-
-            // Add the image to canvas state
-            setImages((prevs) => [
-              ...(prevs ?? []),
-              {
-                id,
-                src: dragUrlRef.current!,
-                x,
-                y,
-                width: width * scale,
-                height: height * scale,
-              },
-            ]);
-
-            setSelectedImage(id);
-          };
-
-          // setImages((prevs) => [
-          //   ...(prevs ?? []),
-          //   {
-          //     id,
-          //     src: imageUrl,
-          //     x,
-          //     y,
-          //   },
-          // ]);
-
-          // setSelectedImage(id);
-          // dragUrlRef.current = undefined;
-        }}
-        onDragOver={(e) => e.preventDefault()}
-      >
-        {editedTextValue.textValue && (
-          <AutosizeInput
-            value={editedTextValue.textValue}
-            onChange={(e) => {
-              setEditedTextValue((prevValues) => ({
-                textValue: e.target.value,
-                x: prevValues.x,
-                y: prevValues.y,
-              }));
-            }}
-            inputRef={(el) => {
-              inputRef.current = el;
-            }}
-            inputStyle={{
-              fontSize: '24px',
-              position: 'absolute',
-              top: `${editedTextValue.y}px`,
-              left: `${editedTextValue.x}px`,
-              lineHeight: 0,
-              zIndex: '2',
-            }}
-            onBlur={() => {
-              setTexts((prevTexts) => {
-                return prevTexts.map((text) => {
-                  if (text.id === selectedTextId) {
-                    return { ...text, text: editedTextValue.textValue };
-                  }
-
-                  return text;
-                });
-              });
-
-              setEditedTextValue({
-                textValue: '',
-                x: 50,
-                y: 50,
-              });
-              setSelectedEditTextId(0);
-              setSelectedImage(0);
-            }}
-          />
-        )}
-        <Stage
-          width={dimensions.width}
-          height={dimensions.height}
-          ref={stageRef}
-          onClick={() => {
-            setSelectedImage(0);
-          }}
-        >
-          <Layer
-            onClick={(e) => {
-              e.cancelBubble = true;
+    <div style={{ display: 'flex', gap: 20 }}>
+      {/* Sidebar */}
+      <div style={{ width: 250, padding: 10, background: '#f3f3f3' }}>
+        <h3>Menu Items</h3>
+        {menuItems.map((product) => (
+          <div
+            key={product.id}
+            draggable
+            onDragStart={(e) =>
+              e.dataTransfer.setData(
+                'application/json',
+                JSON.stringify(product),
+              )
+            }
+            style={{
+              border: '1px solid #ccc',
+              padding: 10,
+              marginBottom: 10,
+              cursor: 'grab',
+              background: 'white',
             }}
           >
-            {images &&
-              images.map((img) => (
-                <>
-                  <URLImage
-                    key={img.id}
-                    image={img}
-                    isSelectedImage={selectedImage === img.id}
-                    onSelectImage={() => {
-                      setSelectedImage(img.id);
-                      setSelectedTextId(0);
-                    }}
-                    onCoordsChange={(newAttrs) => {
-                      setImages((prev) =>
-                        prev?.map((item) =>
-                          item.id === img.id ? { ...item, ...newAttrs } : item,
-                        ),
-                      );
-                    }}
-                  />
-                </>
-              ))}
+            <strong>{product.name}</strong>
+            <br />
+            <span>{product.price}</span>
+          </div>
+        ))}
+      </div>
 
-            {texts.map((txt) => (
-              <Text
-                key={txt.id}
-                text={txt.text}
-                x={txt.x}
-                y={txt.y}
-                visible={txt.id !== selectedEditTextId}
-                fontSize={txt.fontSize}
+      {/* Canvas Wrapper */}
+      <div
+        onDrop={(e) => {
+          const data: Product = JSON.parse(
+            e.dataTransfer.getData('application/json'),
+          );
+          const dropCoord = getPerfectDropCoords(
+            e.nativeEvent.offsetX,
+            e.nativeEvent.offsetY,
+          );
+          setKonvaItems((prev) => [
+            ...prev,
+            { ...data, id: Math.random(), ...dropCoord },
+          ]);
+          setMenuItems((prev) => prev.filter((item) => item.id !== data.id));
+        }}
+        onDragOver={(e) => e.preventDefault()}
+        style={{
+          border: '2px dashed #ccc',
+          background: '#eee',
+          width: canvasWidth,
+          height: canvasHeight,
+        }}
+      >
+        <Stage ref={stageRef} width={canvasWidth} height={canvasHeight}>
+          <Layer>
+            {/* Grid */}
+            {coords.map((coord, i) => (
+              <Group key={i} x={coord.x} y={coord.y}>
+                <Rect
+                  width={CELL_WIDTH}
+                  height={CELL_HEIGHT}
+                  fill="#fafafa"
+                  stroke="#ccc"
+                  dash={[4, 4]}
+                />
+                <Text
+                  text="Place product here"
+                  fontSize={14}
+                  fill="#aaa"
+                  width={CELL_WIDTH}
+                  height={CELL_HEIGHT}
+                  align="center"
+                  verticalAlign="middle"
+                />
+              </Group>
+            ))}
+
+            {/* Dropped Items */}
+            {konvaItems.map((item, idx) => (
+              <Group
+                key={idx}
+                x={item.x}
+                y={item.y}
                 draggable
-                // fill={selectedTextId === txt.id ? 'red' : 'black'}
-                onClick={(e) => {
-                  e.cancelBubble = true;
-                  setSelectedTextId(txt.id);
+                onDragEnd={(e) => handleDragEnd(e, idx)}
+                onDragMove={(e) => {
+                  const { x, y } = e.target.position();
+                  const maxCoord = getPerfectDropCoords(x, y);
+                  snapCoords.current = maxCoord || { x: 0, y: 0 };
                 }}
-                onDblClick={(e) => {
-                  e.cancelBubble = true;
-                  // setEditedText(txt.text);
-
-                  setEditedTextValue({
-                    textValue: txt.text,
-                    x: txt.x,
-                    y: txt.y,
-                  });
-
-                  inputRef.current?.focus();
-                  // setSelectedTextId(txt.id);
-                  setSelectedEditTextId(txt.id);
-                }}
-                onDragEnd={(e) => {
-                  const newX = e.target.x();
-                  const newY = e.target.y();
-                  setTexts((prev) =>
-                    prev.map((t) =>
-                      t.id === txt.id ? { ...t, x: newX, y: newY } : t,
-                    ),
-                  );
-                }}
-              />
+              >
+                <Rect
+                  width={CELL_WIDTH}
+                  height={CELL_HEIGHT}
+                  fill="#fff"
+                  stroke="#333"
+                  shadowBlur={5}
+                  cornerRadius={8}
+                />
+                <Text
+                  text={`${item.name}\n${item.price}`}
+                  fontSize={16}
+                  fill="#222"
+                  padding={10}
+                  width={CELL_WIDTH}
+                />
+              </Group>
             ))}
           </Layer>
         </Stage>
       </div>
     </div>
   );
-};
-
-export default App;
+}
